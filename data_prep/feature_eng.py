771efd52ea06ecd775e79935b70291f1122d6b89
Todd Niven <todd.niven@team.telstra.com>
2019-06-30 10:06:32 +1000

    trying to incorporate playing well vs play not well features

--- data_prep/feature_eng.py
+++ data_prep/feature_eng.py
@@ -16,10 +16,15 @@ class Features:
         df.columns = Features().cols()
         df['perc'] = df['h_perc'] / df['a_perc']
         df['R_mean'] = df['h_R_mean'] / df['a_R_mean']
-        df['F_mean'] = df['h_F_mean'] / df['a_F_mean']
-        df['A_mean'] = df['h_A_mean'] / df['a_A_mean']
-        df['F_std'] = df['h_F_std'] / df['a_F_std']
-        df['A_std'] = df['h_A_std'] / df['a_A_std']
+        df['F_ph_na'] = (df['h_F_mean'] + df['h_F_std']) / (df['a_F_mean'] - df['a_F_std'])
+        df['F_ph_pa'] = (df['h_F_mean'] + df['h_F_std']) / (df['a_F_mean'] + df['a_F_std'])
+        df['F_nh_na'] = (df['h_F_mean'] - df['h_F_std']) / (df['a_F_mean'] - df['a_F_std'])
+        df['F_nh_pa'] = (df['h_F_mean'] - df['h_F_std']) / (df['a_F_mean'] + df['a_F_std'])
+        df['A_nh_pa'] = (df['h_A_mean'] - df['h_A_std']) / (df['a_A_mean'] + df['a_A_std'])
+        df['A_nh_na'] = (df['h_A_mean'] - df['h_A_std']) / (df['a_A_mean'] - df['a_A_std'])
+        df['A_ph_pa'] = (df['h_A_mean'] + df['h_A_std']) / (df['a_A_mean'] + df['a_A_std'])
+        df['A_ph_na'] = (df['h_A_mean'] + df['h_A_std']) / (df['a_A_mean'] - df['a_A_std'])
+        # df = df.drop(['h_F_std', 'h_A_std', 'h_M_std', 'a_F_std', 'a_A_std', 'a_M_std'], axis=1)
         df = df.replace([np.inf, -np.inf], 0)
         df = df.fillna(0)
         return df
